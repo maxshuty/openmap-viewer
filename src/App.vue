733719b2b9f3929897696edd0697ac131bbdee1e
Max Poshusta <MaxPoshusta@gmail.com>
2021-04-18 06:28:16 -0500

    Adding App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <map-comp />
+</template>
+
+<script>
+import MapComp from './components/MapComp.vue';
+export default {
+  name: 'App',
+  components: { MapComp },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
